48dc916a2de98969015ec9318fba359664af2345
AngelaMae_Garrovillas <androiduser1757@gmail.com>
2019-08-29 17:42:52 -0400

    makes trivia struct file

---
+++ Unit-Testing-Lab/TriviaStruct.swift
@@ -0,0 +1,9 @@
+//
+//  TriviaStruct.swift
+//  Unit-Testing-Lab
+//
+//  Created by Angela Garrovillas on 8/29/19.
+//  Copyright © 2019 Angela Garrovillas. All rights reserved.
+//
+
+import Foundation
