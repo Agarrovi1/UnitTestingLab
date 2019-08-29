f574eb5bcda204e9f6f202274905fd28f8510abe
AngelaMae_Garrovillas <androiduser1757@gmail.com>
2019-08-29 17:46:04 -0400

    builds trivia struct

--- Unit-Testing-Lab/TriviaStruct.swift
+++ Unit-Testing-Lab/TriviaStruct.swift
@@ -7,3 +7,46 @@
 //
 
 import Foundation
+enum JSONError: Error {
+    case decodingError(Error)
+}
+
+struct Trivia: Codable {
+    let results: [TriviaInfo]
+    static func getTrivias(from data: Data) throws -> [TriviaInfo] {
+        do {
+            let results = try JSONDecoder().decode(Trivia.self, from: data)
+            return results.results
+        } catch {
+            throw JSONError.decodingError(error)
+        }
+    }
+}
+
+struct TriviaInfo: Codable {
+    let question: String
+    let correct_answer: String
+    let incorrect_answers: [String]
+    
+    func takePercentFromQuestion() -> String {
+        if let question = question.removingPercentEncoding {
+            return question + "?"
+        }
+        return question + "?"
+    }
+    func takePercentFromCorrectAnswer() -> String {
+        if let answer = correct_answer.removingPercentEncoding {
+            return answer
+        }
+        return correct_answer
+    }
+    func takePercentFromIncorrectAnswers() -> [String] {
+        var newArr = [String]()
+        for a in incorrect_answers {
+            if let incorrect = a.removingPercentEncoding {
+                newArr.append(incorrect)
+            }
+        }
+        return newArr
+    }
+}
